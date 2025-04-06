fe4a2d38109fb8dc05889ee9cff3586dc8f421b3
Wafaabahgat <wafaabhgt@gmail.com>
2025-04-06 23:15:37 +0300

    -

---
+++ src/middleware/AuthUser.jsx
@@ -0,0 +1,14 @@
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+const AuthUser = ({ children }) => {
+    const user = cookies.get('user');
+    console.log(user);
+
+    if (user && user.email) {
+        return (window.location.href = '/');
+    }
+    return children;
+};
+
+export default AuthUser;
